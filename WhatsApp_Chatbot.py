ef2619ddc0cd5c13cb3c97faf2f753acfcda17e5
Aryan Wadhwa <aryanwadhwa05@gmail.com>
2023-01-26 12:26:37 +0530

    Intermediate Stage of completion

---
+++ WhatsApp_Chatbot.py
@@ -0,0 +1,75 @@
+import pyautogui as pt
+import pyperclip as pc
+from pynput.mouse import Controller, Button
+from time import sleep
+
+#Mouse click
+mouse = Controller()
+
+class WhatsApp:
+    
+    #Starting Values
+    def __init__(self, speed = .5, click_speed = .5):
+        self.speed = speed
+        self.click_speed = click_speed
+        self.message = ''
+        self.last_message = ''
+    
+    #Navigate to the green dots for new messages
+    def nav_green_dot(self):
+        try:
+            position = pt.locateOnScreen('green_dot.png', confidence = .7)
+            pt.moveTo(position[0:2], duration=self.speed)
+            pt.moveRel(-100, 0, duration = self.speed)
+            pt.doubleClick(interval=self.click_speed)
+        # except Exception as e:
+            # print('Exception (nav_green_dot):', e)
+        except:
+            print("No new messages!!")
+    
+    #Navigate to the text box
+    def nav_input_box(self):
+        try:
+            position = pt.locateOnScreen('paper_clip.png', confidence = .7)
+            pt.moveTo(position[0:2], duration=self.speed)
+            pt.moveRel(100, 25, duration = self.speed)
+            pt.doubleClick(interval=self.click_speed)
+        # except Exception as e:
+            # print('Exception (nav_input_box):', e)
+        except:
+            print("No new messages!!")
+    
+    #Navigate to the message we want to copy
+    def nav_move_to_message(self):
+        try:
+            position = pt.locateOnScreen('paper_clip.png', confidence = .7)
+            pt.moveTo(position[0:2], duration=self.speed)
+            pt.moveRel(-37, -71, duration = self.speed)
+        # except Exception as e:
+            # print('Exception (nav_input_box):', e)
+        except:
+            print("No new messages!!")
+        
+    #Copying the message
+    def get_message(self):
+        mouse.click(Button.left, 3)
+        sleep(self.speed)
+        mouse.click(Button.right, 1)
+        sleep(self.speed)   
+        pt.moveRel(40, -280, duration=self.speed)
+        mouse.click(Button.left, 2)
+        sleep(1)    
+        # sleep(self.speed)
+        self.message = pc.paste()
+        print("The user said: ", self.message)
+        
+    #Sends the message to the user
+
+wa_bot = WhatsApp(speed = .7, click_speed=.8)
+sleep(2)
+# wa_bot.nav_green_dot()
+wa_bot.nav_input_box()
+wa_bot.nav_move_to_message()
+wa_bot.get_message()
+
+
